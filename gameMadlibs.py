d11e069354e46cf206eec92bb139fa2de06e3b9b
saint Claire <saint mbakastclaire@gmail.com>
2025-07-30 21:07:32 +0200

    ajout gameMadlibs.py

---
+++ gameMadlibs.py
@@ -0,0 +1,14 @@
+#Madlibs game
+# jeu de mots où vous créez une histoire
+#en remplissant les blancs avec des mots aléatoires
+
+adjective1 = input("entre un adjectif(description):")
+nom1= input("entrer un nom(personne, place, chose):")
+adjective2 = input("entre un adjectif(description):")
+verb1 = input("entrer un verbe")
+adjective3 = input("entre un adjectif(description):")
+
+print(f"aujourd hui j'etais {adjective1} zoo. ")
+print(f" in an exhibit, i saw a  {nom1} parc. ")
+print(f"{nom1} etait {adjective2} et {verb1} ")
+print(f"I was {adjective3}!")
