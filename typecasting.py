8d64d171f521907be180cd3c6e6b99b718126f71
saint Claire <saint mbakastclaire@gmail.com>
2025-07-30 10:22:22 +0200

    conversion

---
+++ typecasting.py
@@ -0,0 +1,24 @@
+#Typecasting = the prcess of converting a variable from one to another
+#                 str(), int(), float(), bool()
+
+name = "Floussy"
+age = 23
+gpa = 4.5
+is_wealthy = True
+
+
+print(type(name))
+gpa = int(gpa)
+
+print(type(gpa))
+print(gpa)
+
+print(age)
+
+age= str(age)
+
+age +="1"
+print(age)
+
+name = bool(name)
+print(name)
