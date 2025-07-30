c2671cdaf7871b7a2988174f1315e612f03d88ef
saint Claire <saint mbakastclaire@gmail.com>
2025-07-30 20:09:24 +0200

    ajout de exorectangle.py

---
+++ exorectangle.py
@@ -0,0 +1,7 @@
+#Exercice1 rectangle
+
+longueur = float(input("longueur:"))
+largeur = float(input("largeur:"))
+
+area = longueur*largeur 
+print(area)
--- inputf.py
+++ inputf.py
@@ -1,5 +1,9 @@
-#input()= fonction qui permet à l'utilisateur de saisir les données
+name = input("comment tu t'appelles?:")
+age = int(input("Quel age as-tu?"))
+a ="happy birthday frangin"
+age = int(age)
+age = age + 1
+print(f"salut {name}!")
+print(f"tu as {age} ans")
 
 
-name = input("comment tu t'appel?:")
-print(name)
