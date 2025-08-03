6459776d49c5b4a28307546a3e2296503b77ee6e
saint Claire <saint mbakastclaire@gmail.com>
2025-08-03 22:55:58 +0200

    puissnace

---
+++ mathfunction.py
@@ -0,0 +1,12 @@
+friends = 5
+#friends = friends + 1
+#friends += 1
+
+#friends = friends - 2
+#friends -=2
+#friends = friends *3
+#firiends *=3
+#friends /= 2
+friends = friends ** 2 
+
+print(friends)
