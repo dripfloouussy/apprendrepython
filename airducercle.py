70af2b4713054c92bda777add401e8007b562e3c
saint Claire <saint mbakastclaire@gmail.com>
2025-08-04 17:45:29 +0200

    l'aire d'un cercle

---
+++ airducercle.py
@@ -0,0 +1,7 @@
+import math 
+
+rayon = float(input("veuiller entrer le rayon du cercle: "))
+
+area = math.pi * pow(rayon, 2)
+
+print(f"l'aire du cercle est {area}cm²")
--- exercicefounction.py
+++ exercicefounction.py
@@ -6,4 +6,4 @@ rayon = float(input('Enter le rayon du cercle: '))
 circumference = 2 * math.pi * rayon
 
 
-print(f"la circonférence est: {circumference}")
+print(f"la circonférence est: {circumference} cm")
